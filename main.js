f63b8f67cac112c762bfbc3d7008c6325de513f7
Michał Lesiak <m.lesiaq@gmail.com>
2020-05-18 16:58:42 +0200

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-alert('Witaj na mojej stronie!');
+alert("Witaj na mojej stronie!");
 
-const name = 'Michał';
-const age = '29';
+const name = "Michał";
+const age = "29";
 
 console.log(`Nazywam się ${name} i mam ${age} lat.`);
 console.log(age);
