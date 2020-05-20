214c8f855c5b22b28408447c834da4fd0267249d
Michał Lesiak <m.lesiaq@gmail.com>
2020-05-20 23:09:07 +0200

    CSS styles changed, BEM added, new JS addded

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ const age = "29";
 
 console.log(`Nazywam się ${name} i mam ${age} lat.`);
 console.log(age);
+
+const introduction = document.querySelector(".main__introduction--js");
+
+introduction.innerHTML = "Kilka słów o mnie";
